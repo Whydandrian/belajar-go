69648a9ac288695c6101fd5faf1b38860055f176
whydandrian <whydandrian@gmail.com>
2021-06-29 11:03:31 +0700

    add variable.go file & show value variable

---
+++ variable.go
@@ -0,0 +1,16 @@
+// Untuk membuat variable di awali : var variable_name
+
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name ="Wahyudi Andrian"
+	fmt.Println(name)
+	
+	name = "Wahyudi"
+	fmt.Println(name)
+
+}
