da2f7316bd7abad026ee8c1c9393d10c06334cf3
whydandrian <whydandrian@gmail.com>
2021-06-29 11:56:44 +0700

    operation boolean for get nilaiAkhir

--- booleanoperation.go
+++ booleanoperation.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main(){
-	
+	var nilaiakhir = 90
+	var absensi = 80
 
+	var lulusNilaiAkhir bool = nilaiakhir >= 80
+	var nilaiAbsensi bool = absensi >= 80
+
+	var lulus bool = lulusNilaiAkhir && nilaiAbsensi
+
+	fmt.Println(lulus)
 }
