1d8d4692d21a7d1c0e716d6713b5a5bb5695d68c
whydandrian <whydandrian@gmail.com>
2021-06-29 09:21:42 +0700

    add function len to get length string

--- string.go
+++ string.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	// Menampilkan string
 	fmt.Println("Wahyu")
 	fmt.Println("Wahyudi")
 	fmt.Println("Wahyudi Andrian")
+	
+	// function untuk menghitung panjang string
+	fmt.Println(len("Wahyudi Andrian"))
+	fmt.Println("Wahyudi"[0])
+	fmt.Println("Wahyudi Andrian"[1])
+	
+
 }
