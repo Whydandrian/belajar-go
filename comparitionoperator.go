33e3c59e5d1230d5f6aae8801fd0ebb46c621ba4
whydandrian <whydandrian@gmail.com>
2021-06-29 11:50:55 +0700

    add comparation integer value

--- comparitionoperator.go
+++ comparitionoperator.go
@@ -9,5 +9,11 @@ func main() {
 	var result bool = name1 == name2
 
 	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+
+	fmt.Println(value1 >= value2)
+	fmt.Println(value1 <= value2)
 }
 
