79361875e9b58aa9234d35ea94a979fa6ad114e2
whydandrian <whydandrian@gmail.com>
2021-06-29 11:17:17 +0700

    add constant firstName & lastName

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Constant adalah variable yang isinya tidak dapat diubah lagi
+	const firstName string = "Wahyudi"
+	const lastName = "Andrian"
+	const value = 100
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+
+}
