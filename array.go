65b290c0d48ad70e329cd8712984388ef636fcb3
whydandrian <whydandrian@gmail.com>
2021-08-02 17:13:25 +0700

    add slice.go

--- array.go
+++ array.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
-function main() {
-	
+func main() {
+	var names [3]string
+	names[0] = "Wahyudi"
+	names[1] = "Andrian"
+	names[2] = "Ganteng"
 
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
 }
